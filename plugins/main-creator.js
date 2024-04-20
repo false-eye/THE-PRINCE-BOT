d2fe11e3d37d2ffc89b9f10bcfd1f09259748fcc
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-04-20 11:06:18 +0530

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,16 +1,16 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = princeImg.getRandom()	
-const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+const cat = `*_9ine.x3_*
 
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+*_9ine.x3 Owner wa.me/919539901834_*
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
+*_9ine.x3 wa.me/918590717599_*
 *---------------------*
 
 
-*_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
+*_Moderator_*
 *${developer}*`
 
 await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
