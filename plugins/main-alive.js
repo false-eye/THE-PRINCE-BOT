f8148fe024f209b6c886c62c09649425db0aae1f
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-04-20 10:57:47 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -2,10 +2,10 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
     let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://github.com/PRINCE-GDS/THE-PRINCE-BOT/raw/main/Assets/mp3/Audio5.mp3"
-    let url = "https://github.com/PRINCE-GDS/THE-PRINCE-BOT"
-    let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://i.imgur.com/oX4ES3B.jpeg"
+    var vn = "https://github.com/false-eye/THE-PRINCE-BOT/raw/main/Assets/mp3/Audio5.mp3"
+    let url = "https://t.me/sarca_stics"
+    let murl = "https://t.me/sarca_stics"
+    let img = "https://telegra.ph/file/81557b0cd9531411346f3.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -14,15 +14,15 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         mimetype: 'audio/mp4',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
+        fileName: "9ine.x3",
     
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "THE-PRINCE-BOT IS ALIVE",
-          body: "PRINCE BOT",
+          title: "BOT IS ALIVE",
+          body: "9ine.x3",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP',
+          sourceUrl: 'https://t.me/sarca_stics',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
